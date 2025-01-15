7d0e16853e1d243d7c06ba9566669ae9d6f634e8
suranjan2003 <karmakarsuranjan12@gmail.com>
2025-01-15 14:45:48 +0530

    home page initialized

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        <Benefits />
+        {/* <Benefits />
         <Collaboration />
         <Services />
         <Pricing />
         <Roadmap />
-        <Footer />
+        <Footer /> */}
       </div>
 
       <ButtonGradient />
