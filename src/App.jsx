0acde431d4f4425e67e7a304ed1934c55605af54
suranjan2003 <karmakarsuranjan12@gmail.com>
2025-01-15 15:36:29 +0530

    router updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-import ButtonGradient from "./assets/svg/ButtonGradient";
-import Benefits from "./components/Benefits";
+import Benefits from "./components/Events";
 import Collaboration from "./components/Collaboration";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
-import Services from "./components/Services";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-        <Hero />
-        {/* <Benefits />
-        <Collaboration />
-        <Services />
-        <Pricing />
-        <Roadmap />
-        <Footer /> */}
-      </div>
-
-      <ButtonGradient />
-    </>
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/events" element={<Benefits />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/roadmap" element={<Roadmap />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
