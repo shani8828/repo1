a89900a48ca20e025aaf4a3ece815db90d5bd8cc
Nishant Mishra <nishant202mishra@gmail.com>
2025-01-15 16:16:07 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,24 +23,29 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            Welcome&nbsp;to&nbsp;
+            {` `}
             <span className="inline-block relative">
-              Brainwave{" "}
+              <span className="bg-gradient-to-r from-orange-500 to-red-600 text-transparent bg-clip-text">
+                Optima 2025
+              </span>{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
-                width={624}
-                height={28}
+                width={450}
+                height={20}
                 alt="Curve"
               />
             </span>
           </h1>
-          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+
+          <p className="body-1 max-w-5xl mx-auto mb-6 text-grey-500 lg:mb-8 font-semibold">
+            IIT Kharagpur's renowned Techno-Optimization Fest, Optima, is back
+            from 6th-8th March 2025, proudly organized by the Department of
+            Industrial and Systems Engineering.
           </p>
-          <Button href="/pricing" white>
-            Get started
+          <Button href="/" white>
+            Register
           </Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
@@ -92,10 +97,7 @@ const Hero = () => {
 
           <BackgroundCircles />
         </div>
-
-        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
-
       <BottomLine />
     </Section>
   );
