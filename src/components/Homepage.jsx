e2858c1116a372e28c11636c7edec30e6ffa6e8b
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-01-17 18:23:03 +0530

    homepage updated

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,12 @@
 import Hero from "./Hero";
 import FAQs from "./Faqs";
+import HodMessage from "./HodMessage";
 
 const Homepage = () => {
     return (
       <>
          <div><Hero/></div>
-         
+         <HodMessage/>
          <div className="relative z-10">
           <FAQs />
          </div>
