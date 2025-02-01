d7f962137bc6036565cfb5a128a7d4b2e2c1d062
Saharsh Agrawal <saharshagrawal@gmail.com>
2025-02-01 23:33:51 +0530

    Button component <Link> or <a> sorted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import logo from "../assets/optima2025-logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faEnvelope,
-  faLocationDot,
-  faPhone,
+	faEnvelope,
+	faLocationDot,
+	faPhone,
 } from "@fortawesome/free-solid-svg-icons";
 import {
-  faLinkedin,
-  faSquareFacebook,
-  faSquareInstagram,
+	faLinkedin,
+	faSquareFacebook,
+	faSquareInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import "../components/OptimaCss/Footer.css";
 import { Link } from "react-router-dom";
@@ -17,113 +17,130 @@ import { Link } from "react-router-dom";
 import { GradientLight } from "./design/Benefits";
 
 const Footer = () => {
-  return (
-    <div className="relative z-5">
-      <footer>
-        <section id="optima-footer">
-          <div className="optima-footer-about-portion">
-            <a href="/">
-              <img src={logo} alt="logo" />
-            </a>
-            <p className="lg:pr-10 opacity-80">
-              Optima, IIT Kharagpur's Techno-Optimization Fest, fosters
-              innovation, problem-solving, and collaborations, organized by the
-              Department of Industrial and Systems Engineering.
-            </p>
-          </div>
-          <div className="quick-links-contact-us">
-            <div className="optima-footer-quick-links">
-              <h3 className="text-2xl md:text-2.5xl lg:text-2.8xl mb-3 ">
-                QUICK LINKS
-              </h3>
-              <div className="flex gap-[2rem]">
-              <ul>
-                  <li>
-                    <Link to="/about">About Us</Link>
-                  </li>
-                  <li>
-                    <Link to="/gallery">Gallery</Link>
-                  </li>
-                  <li>
-                    <Link to="/workshops">Workshops</Link>
-                  </li>
-                  <li>
-                    <Link to="/competitions">Competitions</Link>
-                  </li>
-                </ul>
-                <ul>
-                  <li>
-                    <Link to="/guest-lectures">Guest Lectures</Link>
-                  </li>
-                  <li>
-                    <Link to="/sponsors">Sponsors</Link>
-                  </li>
-                  <li>
-                    <Link to="/team">Team</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="optima-footer-contact-us">
-              <h3 className="text-2xl md:text-2.5xl lg:text-2.8xl mb-3">
-                CONTACT US
-              </h3>
-              <div>
-                <div className="optima-footer-location pb-1">
-                  <a
-                    className="optima-footer-normal"
-                    href="https://maps.app.goo.gl/7t9fs2Z4KE2Dqjnb6"
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faLocationDot} /> Department of
-                    Industrial and Systems Engineering, IIT Kharagpur - 721302
-                  </a>
-                  <a
-                    className="optima-footer-hovered"
-                    href="https://maps.app.goo.gl/7t9fs2Z4KE2Dqjnb6"
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faLocationDot} bounce /> Department
-                    of Industrial and Systems Engineering, IIT Kharagpur -
-                    721302
-                  </a>
-                </div>
-                <div className="optima-footer-call pb-1">
-                  <a
-                    className="optima-footer-normal"
-                    href={`tel:+916370708967`}
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faPhone} />
-                    <span className="pl-2">+91 6370708967</span>
-                  </a>
-                  <a
-                    className="optima-footer-hovered"
-                    href={`tel:+916370708967`}
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faPhone} shake />
-                    <span className="pl-2">+91 6370708967</span>
-                  </a>
-                </div>
-                <div className="optima-footer-mail1 pb-1">
-                  <a
-                    className="optima-footer-normal"
-                    href={`mailto:sumanta@optima.org.in`}
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faEnvelope} /> sumanta@optima.org.in
-                  </a>
-                  <a
-                    className="optima-footer-hovered"
-                    href={`mailto:sumanta@optima.org.in`}
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faEnvelope} beat />{" "}
-                    sumanta@optima.org.in
-                  </a>
-                </div>
-                {/* <div className="optima-footer-mail2">
+	return (
+		<div className="relative z-5">
+			<footer>
+				<section id="optima-footer">
+					<div className="optima-footer-about-portion">
+						<Link to="/">
+							<img src={logo} alt="logo" />
+						</Link>
+						<p className="lg:pr-10 opacity-80">
+							Optima, IIT Kharagpur's Techno-Optimization Fest,
+							fosters innovation, problem-solving, and
+							collaborations, organized by the Department of
+							Industrial and Systems Engineering.
+						</p>
+					</div>
+					<div className="quick-links-contact-us">
+						<div className="optima-footer-quick-links">
+							<h3 className="text-2xl md:text-2.5xl lg:text-2.8xl mb-3 ">
+								QUICK LINKS
+							</h3>
+							<div className="flex gap-[2rem]">
+								<ul>
+									<li>
+										<Link to="/about">About Us</Link>
+									</li>
+									<li>
+										<Link to="/gallery">Gallery</Link>
+									</li>
+									<li>
+										<Link to="/workshops">Workshops</Link>
+									</li>
+									<li>
+										<Link to="/competitions">
+											Competitions
+										</Link>
+									</li>
+								</ul>
+								<ul>
+									<li>
+										<Link to="/guest-lectures">
+											Guest Lectures
+										</Link>
+									</li>
+									<li>
+										<Link to="/sponsors">Sponsors</Link>
+									</li>
+									<li>
+										<Link to="/team">Team</Link>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div className="optima-footer-contact-us">
+							<h3 className="text-2xl md:text-2.5xl lg:text-2.8xl mb-3">
+								CONTACT US
+							</h3>
+							<div>
+								<div className="optima-footer-location pb-1">
+									<a
+										className="optima-footer-normal"
+										href="https://maps.app.goo.gl/7t9fs2Z4KE2Dqjnb6"
+										target="_blank"
+									>
+										<FontAwesomeIcon icon={faLocationDot} />{" "}
+										Department of Industrial and Systems
+										Engineering, IIT Kharagpur - 721302
+									</a>
+									<a
+										className="optima-footer-hovered"
+										href="https://maps.app.goo.gl/7t9fs2Z4KE2Dqjnb6"
+										target="_blank"
+									>
+										<FontAwesomeIcon
+											icon={faLocationDot}
+											bounce
+										/>{" "}
+										Department of Industrial and Systems
+										Engineering, IIT Kharagpur - 721302
+									</a>
+								</div>
+								<div className="optima-footer-call pb-1">
+									<a
+										className="optima-footer-normal"
+										href={`tel:+916370708967`}
+										target="_blank"
+									>
+										<FontAwesomeIcon icon={faPhone} />
+										<span className="pl-2">
+											+91 6370708967
+										</span>
+									</a>
+									<a
+										className="optima-footer-hovered"
+										href={`tel:+916370708967`}
+										target="_blank"
+									>
+										<FontAwesomeIcon icon={faPhone} shake />
+										<span className="pl-2">
+											+91 6370708967
+										</span>
+									</a>
+								</div>
+								<div className="optima-footer-mail1 pb-1">
+									<a
+										className="optima-footer-normal"
+										href={`mailto:sumanta@optima.org.in`}
+										target="_blank"
+									>
+										<FontAwesomeIcon icon={faEnvelope} />{" "}
+										sumanta@optima.org.in
+									</a>
+									<a
+										className="optima-footer-hovered"
+										href={`mailto:sumanta@optima.org.in`}
+										target="_blank"
+									>
+										<FontAwesomeIcon
+											icon={faEnvelope}
+											beat
+										/>{" "}
+										sumanta@optima.org.in
+									</a>
+								</div>
+								{/* <div className="optima-footer-mail2">
                   <a
                     className="optima-footer-normal"
                     href={`mailto:suranjan.karmakar.iitkgp@gmail.com`}
@@ -141,66 +158,74 @@ const Footer = () => {
                     suranjan.karmakar.iitkgp@gmail.com
                   </a>
                 </div> */}
-              </div>
-              <div className="optima-footer-social-handles">
-                <div className="optima-footer-fb">
-                  <a
-                    className="optima-footer-normal"
-                    href="https://www.facebook.com/optima.iitkharagpur"
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faSquareFacebook} />
-                  </a>
-                  <a
-                    className="optima-footer-hovered"
-                    href="https://www.facebook.com/optima.iitkharagpur"
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faSquareFacebook} />
-                  </a>
-                </div>
-                <div className="optima-footer-ig">
-                  <a
-                    className="optima-footer-normal"
-                    href="https://www.instagram.com/optima.iitkgp?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faSquareInstagram} />
-                  </a>
-                  <a
-                    className="optima-footer-hovered"
-                    href="https://www.instagram.com/optima.iitkgp?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faSquareInstagram} />
-                  </a>
-                </div>
-                <div className="optima-footer-in">
-                  <a
-                    className="optima-footer-normal"
-                    href="https://www.linkedin.com/company/optima-iitkgp/"
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faLinkedin} />
-                  </a>
-                  <a
-                    className="optima-footer-hovered"
-                    href="https://www.linkedin.com/company/optima-iitkgp/"
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon icon={faLinkedin} />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <div className="text-xs sm:text-sm md:text-md lg:text-lg text-gray-400 text-center">
-          &copy; Optima 2025 | All Rights Reserved.
-        </div>
-      </footer>
-    </div>
-  );
+							</div>
+							<div className="optima-footer-social-handles">
+								<div className="optima-footer-fb">
+									<a
+										className="optima-footer-normal"
+										href="https://www.facebook.com/optima.iitkharagpur"
+										target="_blank"
+									>
+										<FontAwesomeIcon
+											icon={faSquareFacebook}
+										/>
+									</a>
+									<a
+										className="optima-footer-hovered"
+										href="https://www.facebook.com/optima.iitkharagpur"
+										target="_blank"
+									>
+										<FontAwesomeIcon
+											icon={faSquareFacebook}
+										/>
+									</a>
+								</div>
+								<div className="optima-footer-ig">
+									<a
+										className="optima-footer-normal"
+										href="https://www.instagram.com/optima.iitkgp?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+										target="_blank"
+									>
+										<FontAwesomeIcon
+											icon={faSquareInstagram}
+										/>
+									</a>
+									<a
+										className="optima-footer-hovered"
+										href="https://www.instagram.com/optima.iitkgp?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+										target="_blank"
+									>
+										<FontAwesomeIcon
+											icon={faSquareInstagram}
+										/>
+									</a>
+								</div>
+								<div className="optima-footer-in">
+									<a
+										className="optima-footer-normal"
+										href="https://www.linkedin.com/company/optima-iitkgp/"
+										target="_blank"
+									>
+										<FontAwesomeIcon icon={faLinkedin} />
+									</a>
+									<a
+										className="optima-footer-hovered"
+										href="https://www.linkedin.com/company/optima-iitkgp/"
+										target="_blank"
+									>
+										<FontAwesomeIcon icon={faLinkedin} />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<div className="text-xs sm:text-sm md:text-md lg:text-lg text-gray-400 text-center">
+					&copy; Optima 2025 | All Rights Reserved.
+				</div>
+			</footer>
+		</div>
+	);
 };
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,73 +9,80 @@ import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
 
 const Header = () => {
-  const location = useLocation();
-  const [openNavigation, setOpenNavigation] = useState(false);
+	const location = useLocation();
+	const [openNavigation, setOpenNavigation] = useState(false);
 
-  const toggleNavigation = () => {
-    if (openNavigation) {
-      setOpenNavigation(false);
-      enablePageScroll();
-    } else {
-      setOpenNavigation(true);
-      disablePageScroll();
-    }
-  };
+	const toggleNavigation = () => {
+		if (openNavigation) {
+			setOpenNavigation(false);
+			enablePageScroll();
+		} else {
+			setOpenNavigation(true);
+			disablePageScroll();
+		}
+	};
 
-  const handleClick = () => {
-    if (!openNavigation) return;
+	const handleClick = () => {
+		if (!openNavigation) return;
 
-    enablePageScroll();
-    setOpenNavigation(false);
-  };
+		enablePageScroll();
+		setOpenNavigation(false);
+	};
 
-  return (
-    <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
-        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
-      }`}
-    >
-      <div className="flex items-center justify-between px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="flex items-center gap-1 w-[15rem] xl:mr-8" href="/">
-          <img src={logo} alt="Optima" className="w-[200px] md:w-[230px]" />
-        </a>
-        <nav
-          className={`${
-            openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:bg-transparent`}
-        >
-          <div className="relative z-2 flex flex-col items-start justify-center m-auto lg:flex-row lg:justify-end">
-            {navigation.map((item) => (
-              <Link
-                key={item.id}
-                to={item.url}
-                onClick={handleClick}
-                className={`block relative font-code text-2xl uppercase transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg:hidden" : ""
-                } px-3 xl:px-4 py-5 md:py-6 lg:-mr-0.25 lg:text-sm lg:font-semibold ${
-                  location.pathname === item.url // Check if the current page matches the URL
-                    ? "text-color-1" // Apply purple color for active page
-                    : "text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1`}
-              >
-                {item.title}
-              </Link>
-            ))}
-          </div>
+	return (
+		<div
+			className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+				openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+			}`}
+		>
+			<div className="flex items-center justify-between px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+				<Link
+					className="flex items-center gap-1 w-[15rem] xl:mr-8"
+					to="/"
+				>
+					<img
+						src={logo}
+						alt="Optima"
+						className="w-[200px] md:w-[230px]"
+					/>
+				</Link>
+				<nav
+					className={`${
+						openNavigation ? "flex" : "hidden"
+					} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:bg-transparent`}
+				>
+					<div className="relative z-2 flex flex-col items-start justify-center m-auto lg:flex-row lg:justify-end">
+						{navigation.map((item) => (
+							<Link
+								key={item.id}
+								to={item.url}
+								onClick={handleClick}
+								className={`block relative font-code text-2xl uppercase transition-colors hover:text-color-1 ${
+									item.onlyMobile ? "lg:hidden" : ""
+								} px-3 xl:px-4 py-5 md:py-6 lg:-mr-0.25 lg:text-sm lg:font-semibold ${
+									location.pathname === item.url // Check if the current page matches the URL
+										? "text-color-1" // Apply purple color for active page
+										: "text-n-1/50"
+								} lg:leading-5 lg:hover:text-n-1`}
+							>
+								{item.title}
+							</Link>
+						))}
+					</div>
 
-          <HamburgerMenu />
-        </nav>
+					<HamburgerMenu />
+				</nav>
 
-        <Button
-          className="ml-auto lg:hidden"
-          px="px-3"
-          onClick={toggleNavigation}
-        >
-          <MenuSvg openNavigation={openNavigation} />
-        </Button>
-      </div>
-    </div>
-  );
+				<Button
+					className="ml-auto lg:hidden"
+					px="px-3"
+					onClick={toggleNavigation}
+				>
+					<MenuSvg openNavigation={openNavigation} />
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
