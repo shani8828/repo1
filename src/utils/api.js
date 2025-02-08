18d799daa7b3fb38ebe3e8e165010e8b906758aa
suranjan2003 <karmakarsuranjan12@gmail.com>
2025-02-08 16:18:06 +0530

    .env file

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Access environment va
 
 export const submitQuery = async (queryData) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/query`, queryData);
+    const response = await axios.post(`${API_BASE_URL}/admin/query`, queryData);
     return response.data;
   } catch (error) {
     console.error("Error submitting query:", error);
