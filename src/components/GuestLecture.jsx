f1abc22a53bf930a293e377f4c0d385fa789a3fb
suranjan2003 <karmakarsuranjan12@gmail.com>
2025-02-27 23:03:04 +0530

    2 more gl added

--- src/components/GuestLecture.jsx
+++ src/components/GuestLecture.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Section from "./Section";
 import Heading from "./Heading";
 import Button from "./Button";
-import ComingSoon from "./design/ComingSoon";
 import { BackgroundCircles } from "./design/Hero";
 import { Gradient } from "./design/Services";
 import { GradientLight } from "./design/Benefits";
@@ -12,122 +11,168 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { motion } from "framer-motion";
 
 const GuestLecture = () => {
-	document.title = "Guest Lectures | Optima 2025";
-	const text = "Many-More-Stars-Are-On-Their-Way!";
-	const chars = text.split("");
-	return (
-		<Section id="guestLecture">
-			<div className="container relative z-2">
-				<Heading
-					className="md:max-w-md lg:max-w-2xl text-center"
-					title="Guest Lectures"
-				/>
-				<BackgroundCircles />
+  document.title = "Guest Lectures | Optima 2025";
+  const text = "Many-More-Stars-Are-On-Their-Way!";
+  const chars = text.split("");
 
-				{/* <div className='flex justify-center'>
-          <ComingSoon link="https://2023.optima.org.in/guest-lectures"/>
-        </div> */}
+  return (
+    <Section id="guestLecture">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl text-center"
+          title="Guest Lectures"
+        />
+        <BackgroundCircles />
 
-				<div className="grid grid-cols-1 gap-8">
-					{guestLectures.map((lecture, index) => (
-						<div
-							key={index}
-							className="md:flex bg-gray-800 rounded-2xl overflow-hidden shadow-xl"
-						>
-							<div className="relative md:w-1/3">
-								<img
-									src={lecture.imgUrl}
-									alt={lecture.name}
-									className="object-cover w-full h-full"
-								/>
-								<div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent md:opacity-50"></div>
-							</div>
+        <div className="grid grid-cols-1 gap-8">
+          {guestLectures.map((lecture, index) => (
+            <div
+              key={index}
+              className="md:flex bg-gray-800 rounded-2xl overflow-hidden shadow-xl"
+            >
+              <div className="relative md:w-1/3">
+                <img
+                  src={lecture.imgUrl}
+                  alt={lecture.name}
+                  className="object-cover w-full h-full"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent md:opacity-50"></div>
+              </div>
 
-							<div className="p-6 md:w-2/3">
-								<h3 className="text-2xl lg:text-4xl font-bold text-white mb-2">
-									{lecture.name}
-								</h3>
-								<p className="text-lg lg:text-2xl text-gray-300 mb-2">
-									{lecture.designation}
-									{lecture.company && `, ${lecture.company}`}
-								</p>
-								<p className="text-gray-400 lg:text-lg mb-2">
-									Topic of Discussion :{" "}
-									<span className="text-purple-300">{lecture.topic}</span>
-								</p>
-								<p className="text-gray-400 lg:text-lg mb-2">{lecture.desc}</p>
-								<div className="flex mb-4 lg:mb-6">
-									{lecture.date && (
-										<p className="text-lg lg:text-2xl text-gray-300">
-											{lecture.date}
-										</p>
-									)}
-									{lecture.mode && lecture.date && (
-										<p className="text-lg lg:text-2xl text-gray-300 mx-2">
-											,
-										</p>
-									)}
-									{lecture.mode && (
-										<p className="text-lg lg:text-2xl text-gray-300">
-											{lecture.mode}
-										</p>
-									)}
-								</div>
-								<div className="flex flex-wrap gap-4">
-									{lecture.linkedin && (
-										<button
-											onClick={() => window.open(lecture.linkedin, "_blank")}
-											className="px-6 py-3 bg-purple-600 hover:bg-purple-700 text-white rounded-lg font-medium flex items-center justify-center transform hover:-translate-y-1 transition-all duration-200"
-										>
-											Know More
-										</button>
-									)}
-									{lecture.companyUrl && (
-										<button
-											onClick={() => window.open(lecture.companyUrl, "_blank")}
-											className="px-6 py-3 bg-transparent border-2 border-purple-600 text-purple-300 hover:bg-purple-600 hover:text-white rounded-lg font-medium flex items-center justify-center transform hover:-translate-y-1 transition-all duration-200"
-										>
-											Visit Company
-										</button>
-									)}
-								</div>
-							</div>
-						</div>
-					))}
+              <div className="p-6 md:w-2/3">
+                <h3 className="text-2xl lg:text-4xl font-bold text-white mb-2">
+                  {lecture.name}
+                </h3>
+                <p className="text-lg lg:text-2xl text-gray-300 mb-2">
+                  {lecture.designation}
+                  {lecture.company && `, ${lecture.company}`}
+                </p>
+                {lecture.topic && <p className="text-gray-400 lg:text-lg mb-2">
+                  Topic of Discussion :{" "}
+                  <span className="text-purple-300">{lecture.topic}</span>
+                </p>}
+                <LectureDescription desc={lecture.desc} />
+                <div className="flex mb-4 lg:mb-6">
+                  {lecture.date && (
+                    <p className="text-lg lg:text-2xl text-gray-300">
+                      {lecture.date}
+                    </p>
+                  )}
+                  {lecture.mode && lecture.date && (
+                    <p className="text-lg lg:text-2xl text-gray-300 mx-2">
+                      ,
+                    </p>
+                  )}
+                  {lecture.mode && (
+                    <p className="text-lg lg:text-2xl text-gray-300">
+                      {lecture.mode}
+                    </p>
+                  )}
+                </div>
+                <div className="flex flex-wrap gap-4">
+                  {lecture.linkedin && (
+                    <button
+                      onClick={() => window.open(lecture.linkedin, "_blank")}
+                      className="px-6 py-3 bg-purple-600 hover:bg-purple-700 text-white rounded-lg font-medium flex items-center justify-center transform hover:-translate-y-1 transition-all duration-200"
+                    >
+                      Know More
+                    </button>
+                  )}
+                  {lecture.companyUrl && (
+                    <button
+                      onClick={() => window.open(lecture.companyUrl, "_blank")}
+                      className="px-6 py-3 bg-transparent border-2 border-purple-600 text-purple-300 hover:bg-purple-600 hover:text-white rounded-lg font-medium flex items-center justify-center transform hover:-translate-y-1 transition-all duration-200"
+                    >
+                      Visit Company
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
 
-					{/* waiting animation */}
-					<div className="flex flex-col items-center justify-center mb-20">
-						<DotLottieReact
-							// src="https://lottie.host/1107f092-3b60-46c8-9844-27bf4944a9e0/6stHI9nDEV.lottie"
-							className="w-4/5 md:w-1/2"
-							src="https://lottie.host/b3097079-bc6f-47ab-9c27-f39e395e8e83/2ptNd6xx7N.lottie"
-							loop
-							autoplay
-						/>
-						<h1 className="z-10 lg:-mt-12 text-md md:text-4xl lg:text-5xl italic font-bold flex">
-							{chars.map((char, index) => (
-								<motion.span
-									key={index}
-									initial={{ y: 0 }}
-									animate={{ y: [-5, 5, -5] }} // Wave effect
-									transition={{
-										duration: 1.5,
-										repeat: Infinity,
-										repeatType: "reverse",
-										delay: (chars.length - index) * 0.1, // Creates the wave effect
-									}}
-									className="inline-block"
-								>
-									{char}
-								</motion.span>
-							))}
-						</h1>
-					</div>
-				</div>
-			</div>
-			<Gradient />
-		</Section>
-	);
+          {/* waiting animation */}
+          <div className="flex flex-col items-center justify-center mb-20">
+            <DotLottieReact
+              className="w-4/5 md:w-1/2"
+              src="https://lottie.host/b3097079-bc6f-47ab-9c27-f39e395e8e83/2ptNd6xx7N.lottie"
+              loop
+              autoplay
+            />
+            <h1 className="z-10 lg:-mt-12 text-md md:text-4xl lg:text-5xl italic font-bold flex">
+              {chars.map((char, index) => (
+                <motion.span
+                  key={index}
+                  initial={{ y: 0 }}
+                  animate={{ y: [-5, 5, -5] }} // Wave effect
+                  transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                    delay: (chars.length - index) * 0.1, // Creates the wave effect
+                  }}
+                  className="inline-block"
+                >
+                  {char}
+                </motion.span>
+              ))}
+            </h1>
+          </div>
+        </div>
+      </div>
+      <Gradient />
+    </Section>
+  );
 };
+
+const LectureDescription = ({ desc }) => {
+	const [showFull, setShowFull] = useState(false);
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	const characterLimit = windowWidth >= 768 ? 600 : 400; // Responsive character limit
+  
+	useEffect(() => {
+	  const handleResize = () => setWindowWidth(window.innerWidth);
+	  window.addEventListener("resize", handleResize);
+	  return () => window.removeEventListener("resize", handleResize);
+	}, []);
+  
+	const toggleShowFull = () => setShowFull(!showFull);
+  
+	const getDescription = () => {
+	  if (showFull) {
+		return desc;
+	  }
+	  
+	  // Check both line count and character count
+	  const lines = desc.split("\n");
+	  const previewLines = windowWidth >= 768 ? 7 : 10;
+	  
+	  if (lines.length <= previewLines && desc.length <= characterLimit) {
+		return desc;
+	  }
+	  
+	  if (lines.length > previewLines) {
+		return lines.slice(0, previewLines).join("\n");
+	  }
+	  
+	  return desc.substring(0, characterLimit);
+	};
+  
+	return (
+	  <div>
+		<p className="text-gray-400 lg:text-lg mb-2 whitespace-pre-line">
+		  {getDescription()}
+		{(desc.split("\n").length > (windowWidth >= 768 ? 7 : 10) || desc.length > characterLimit) && (
+			<button
+			onClick={toggleShowFull}
+			className="text-purple-400 hover:text-purple-300"
+			>
+			{showFull ? " ...Read Less" : " ...Read More"}
+		  </button>
+		)}
+		</p>
+	  </div>
+	);
+  };
 
 export default GuestLecture;
--- src/constants/GuestLectures.js
+++ src/constants/GuestLectures.js
@@ -1,5 +1,7 @@
 import img1 from "../assets/GuestLectures/Rajesh-Kumar.jpg";
 import img2 from "../assets/GuestLectures/Arvind-Kumar.jpeg";
+import img3 from "../assets/GuestLectures/Nitish_kumar.jpg";
+import img4 from "../assets/GuestLectures/ani_pic.png";
 
 export const guestLectures = [
   {
@@ -23,6 +25,30 @@ export const guestLectures = [
     desc: "Dr. Arvind Kumar is a seasoned Operations Research practitioner with over 20 years of experience in enabling large logistics, transportation and e-mobility companies to leverage power of analytics for better planning and execution. Besides his previous leadership role of VP - Research and Development at Optym, he has also advised several startups and small businesses in converting business problems to mathematical formulation, and mature the process of applying OR tools to solve these problems in real-life environment. He holds a BTech in Chemical Engg from IIT Chennai, and a PhD in Industrial & Systems Engg from University of Florida. He is also recipient of best paper awards in the field of medical and military applications at INFORMS.",
     linkedin: "https://www.linkedin.com/in/arviphd/",
     companyUrl: "https://www.optym.com/",
+    date: null,
+    mode: null,
+  },
+  {
+    imgUrl: img3,
+    name: "Nitish Kumar",
+    designation: "Head Analyst",
+    company: "Tata Steel",
+    topic: "Predictive modelling",
+    desc: "Nitish Kumar is the Head Analyst for Manufacturing at Tata Steel, with over 11 years of experience in data science, machine learning, and predictive analytics. He holds a Bachelor of Engineering degree in Computer Science from BIT Mesra (2013). He began his career at Tata Steel in 2013 as a Management Trainee and contributed to the IT team from 2016 to 2018. Following a company-sponsored training program in Data Science, he transitioned to the Data Analytics Team in 2018. His professional interests lie in data science and optimization modeling, and he has a proven track record of developing and implementing data-driven solutions across various business areas, including raw materials, iron making, procurement, and supply chain. Notable achievements in his career include leading a cross-functional team for the Integrated Margin Management project, which spans multiple value chains such as coal, metallics, flux, and ferro alloys. He has also developed price prediction models for key commodities like coal, zinc, and Ferroshot, created plant digital twins to optimize operations, and completed over 50 EBITDA projects utilizing data analytics models across different locations. Additionally, he has filed four patents in the last three years. Beyond his professional endeavors, he is passionate about chess, podcasts, and travel.",
+    linkedin: "https://www.linkedin.com/in/nitish-kumar-446b4258/",
+    companyUrl: "https://www.tatasteel.com/",
+    date: null,
+    mode: null,
+  },
+  {
+    imgUrl: img4,
+    name: "Dr. Anindita Desarkar",
+    designation: "Director",
+    company: "LTIMindtree",
+    topic: "Revolutionizing Optimization using Generative AI",
+    desc: "Dr. Anindita Desarkar (Director - LTIMindtree), An IT Professional having 20 years' experience, working predominantly on Analytics and Machine Learning domain; expert areas include Generative AI, NLP, Predictive Maintenance and AI based Optimization. A dedicated researcher, PhD on “Machine Learning & Analytics” from Jadavpur University having 22+ publications in various SCOPUS indexed journals and conferences. Filed 5 patents in Optimization and AI domain.",
+    linkedin: "https://www.linkedin.com/in/anindita-desarkar-phd-b9a127218?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    companyUrl: "https://www.ltimindtree.com/",
     date: null,
     mode: null,
   },
