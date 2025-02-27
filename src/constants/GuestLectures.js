53bbcffefbca007138e24050dd497a5f085688bc
suranjan2003 <karmakarsuranjan12@gmail.com>
2025-02-28 02:53:38 +0530

    update

--- src/constants/GuestLectures.js
+++ src/constants/GuestLectures.js
@@ -7,10 +7,10 @@ export const guestLectures = [
   {
     imgUrl: img1,
     name: "Dr. Rajesh Kumar",
-    designation: "Director, Data Science",
+    designation: "Director, Business Analytics",
     company: "Philips",
     topic: "Driving Supply Chain digital transformation through data Analytics",
-    desc: "Dr. Rajesh Kumar is the Director for Data Science within Philips integrated supply chain digital center of excellence. He has 20+ years of experience spanning across organizations like SCG Chemicals, DXC Technologies, HP, i2 Technologies, EY and Volvo-Eicher. He holds a degree in mechanical engineering from NIT Bhopal and a masters and PhD from Indian Institute of Science Bangalore.",
+    desc: "Dr. Rajesh Kumar is the Director of Business Analytics at Philips integrated supply chain digital center of excellence. He has 20+ years of experience spanning across organizations like SCG Chemicals, DXC Technologies, HP, i2 Technologies, EY and Volvo-Eicher. He holds a degree in mechanical engineering from NIT Bhopal and a masters and PhD from Indian Institute of Science Bangalore.",
     linkedin: "https://www.linkedin.com/in/dr-rajesh-kumar-0b86a38/",
     companyUrl: "https://www.philips.co.in/",
     date: null,
